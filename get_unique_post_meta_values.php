511e6a54a4a2f8a7b6266a25a82184aa48f800dd
Chinmoy Paul <phpwebdeveloper29@gmail.com>
2014-08-07 12:35:12 +0530

    Update get_unique_post_meta_values.php

--- get_unique_post_meta_values.php
+++ get_unique_post_meta_values.php
@@ -1,9 +1,17 @@
 <?php
- // When you will add the code in your file, you'll remove opening PHP tag
+ /* Don't include the opening PHP tag*/
 /**
  * Description: Getting all the values associated with a specific custom post meta key, across all posts
  * Author: Chinmoy Paul
  * Author URL: http://pwdtechnology.com
+ * 
+ * @param string $key Post Meta Key.
+ * 
+ * @param string $type Post Type. Default is post. You can pass custom post type here.
+ * 
+ * @param string $status Post Status like Publish, draft, future etc. default is publish
+ * 
+ * @return array
  */
  
  function get_unique_post_meta_values( $key = '', $type = 'post', $status = 'publish' ) {
